e8dd845f815434bf8dce8a66af61ce4b69ad1882
Costello90 <alextasssss@gmail.com>
2023-10-20 16:28:13 +0300

    setting up the project structure

--- t_w_wht_agency/urls.py
+++ t_w_wht_agency/urls.py
@@ -1,7 +1,14 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('v1/', include('user.urls')),
+    path('v1/', include('command.urls')),
 ]
+
+# Include URL patterns for API documentation
+urlpatterns += doc_urls
